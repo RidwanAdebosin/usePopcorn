611086fcf3b6e80e3bf2d0c3ef3e6b058aeba29e
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-02-06 22:45:49 +0100

    Text Expander added

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={10}
-        onSetMovieRating={setMovieRating}
-      />
-      <p>Your movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+import TextExpander from "./TextExpander";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         color="blue"
+//         maxRating={10}
+//         onSetMovieRating={setMovieRating}
+//       />
+//       <p>Your movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    {/* <StarRating maxRating={10} /> */}
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <TextExpander />
   </React.StrictMode>
 );
+
+// {/* <App /> */}
+// {/* <StarRating
+//   maxRating={5}
+//   messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+// /> */}
+// {/* <StarRating maxRating={10} /> */}
+// {/* <StarRating size={24} color="red" className="test" defaultRating={3} />
+// <Test /> */}
